4a29fd7c5f346fadc67f46976ec8f33b74f4acd2
astroJeff <andrews@astro.columbia.edu>
2017-03-30 18:52:55 +0300

    Added HMXB test to dart_board/examples/

--- examples/test_HMXB.py
+++ examples/test_HMXB.py
@@ -1,8 +1,47 @@
+import sys
 import numpy as np
+import time
 
+
+sys.path.append("../pyBSE/")
+import pybse
 import dart_board
 
-pub = dart_board.DartBoard("HMXB")
+pub = dart_board.DartBoard("HMXB", evolve_binary=pybse.evolv_wrapper)
 pub.aim_darts()
 
-print(pub.p0)
+
+start_time = time.time()
+pub.throw_darts(nburn=50000, nsteps=50000)
+print("Simulation took",time.time()-start_time,"seconds.")
+
+print("Acceptance fractions:",pub.sampler.acceptance_fraction)
+# print(pub.sampler.flatlnprobability)
+
+
+
+
+
+
+
+
+# Create a corner plot to show the posterior distribution
+import corner
+import matplotlib.pyplot as plt
+from matplotlib import font_manager
+
+fontProperties = {'family':'serif', 'serif':['Times New Roman'], 'weight':'normal', 'size':12}
+ticks_font = font_manager.FontProperties(family='Times New Roman', style='normal', \
+                                         weight='normal', stretch='normal', size=10)
+plt.rc('font', **fontProperties)
+
+# Corner plot
+
+labels = [r"$M_{\rm 1, i}\ (M_{\odot})$", r"$M_{\rm 2, i}\ (M_{\odot})$", r"$a_{\rm i}\ (R_{\odot})$", \
+          r"$e_{\rm i}$", r"$v_{\rm k, i}\ ({\rm km}\ {\rm s}^{-1})$", r"$\theta_{\rm k}\ ({\rm rad.})$", \
+          r"$\delta_{\rm i}\ ({\rm deg.}) $", r"$t_{\rm i}\ ({\rm Myr})$"]
+plt_range = ([7,24], [2.5,15], [0,1500], [0,1], [0,450], [np.pi/4.,np.pi], [0,np.pi], [0,70])
+hist2d_kwargs = {"plot_datapoints" : False}
+corner.corner(pub.sampler.flatchain, labels=labels, range=plt_range, bins=20, max_n_ticks=4, **hist2d_kwargs)
+
+plt.savefig("../figures/test_HMXB_corner.pdf")
