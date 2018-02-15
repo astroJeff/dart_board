ad6b6e07f963041d00dbc3b408da33572c527184
astroJeff <andrews@astro.columbia.edu>
2018-02-15 17:43:42 +0200

    Calculate derived outputs for mock_3

--- examples/mock_3_high.py
+++ examples/mock_3_high.py
@@ -37,6 +37,37 @@ pub.throw_darts(nburn=2, nsteps=52000)
 print("Simulation took",time.time()-start_time,"seconds.")
 
 
+# Since emcee_PT does not have a blobs function, we must include the following calculation
+if pub.chains.ndim == 4:
+
+    print("Generating derived values...")
+
+    ntemps, nchains, nsteps, nvar = pub.chains.shape
+    pub.derived = np.zeros(shape=(ntemps, nchains, nsteps, 9))
+
+    for i in range(ntemps):
+        for j in range(nchains):
+            for k in range(nsteps):
+
+                x_i = pub.chains[i,j,k]
+
+                ln_M1, ln_M2, ln_a, ecc, v_kick_1, theta_kick_1, phi_kick_1, ra, dec, ln_t = x_i
+                M1 = np.exp(ln_M1)
+                M2 = np.exp(ln_M2)
+                a = np.exp(ln_a)
+                time = np.exp(ln_t)
+
+                P_orb = dart_board.posterior.A_to_P(M1, M2, a)
+
+                output = pybse.evolve(M1, M2, P_orb, ecc, v_kick_1, theta_kick_1, phi_kick_1,
+                                      v_kick_1, theta_kick_1, phi_kick_1,
+                                      time, LMC_metallicity, False)
+
+                pub.derived[i,j,k] = np.array([output])
+
+    print("...finished.")
+
+
 # Acceptance fraction
 print("Acceptance fractions:",pub.sampler.acceptance_fraction)
 
