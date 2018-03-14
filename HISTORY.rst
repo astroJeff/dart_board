71405a81a63c848b744ad25eaf73c492baa4887b
astroJeff <andrews@astro.columbia.edu>
2018-03-14 13:00:20 +0200

    Added changelog

---
+++ HISTORY.rst
@@ -0,0 +1,12 @@
+.. :changelog:
+
+1.1.0 (2018-03-14)
+------------------
+
+- pyBSE now outputs a numpy array that includes T_eff, radius, and luminosity.
+
+
+1.0.1 (2018-03-08)
+------------------
+
+- Initial Release
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='dart_board',
-      version='1.0.0',
+      version='1.1.0',
       description='Binary Population Synthesis with MCMC',
       url='http://github.com/astroJeff/dart_board',
       author='Jeff J. Andrews',
